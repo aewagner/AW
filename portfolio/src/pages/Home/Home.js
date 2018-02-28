ecaca3c49a3d31c8197af2af45ba0773308c70ec
Austin Wagner <austin.e.wagner@gmail.com>
2018-02-27 21:52:56 -0800

    basic home page setup

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -1,37 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './Home.css'
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div>
-                <header className="masthead">
-                    <div className="intro-body">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-8 mx-auto">
-                                    <h1 className="brand-heading">Grayscale</h1>
-                                    <p className="intro-text">A free, responsive, one page Bootstrap theme.
-                                         {/* <br>Created by Start Bootstrap.</br> */}
-                                    </p>
-                                    <a href="#about" className="btn btn-circle js-scroll-trigger">
-                                        <i className="fa fa-angle-double-down animated"></i>
-                                    </a>
-                                </div>
-                            </div>
+const Home = () => (
+    <div id="gradient">
+        <header className="text-center">
+            <div className="intro-body">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h1 className="brand-heading">Austin Wagner</h1>
+                            <p className="intro-text">Full-Stack Web Developer.</p>
+                            <a href="#about" className="btn btn-circle js-scroll-trigger">
+                                <i className="fa fa-angle-double-down animated"></i>
+                            </a>
                         </div>
                     </div>
-                </header>
+                </div>
             </div>
-        )
-    }
-
-
-}
+        </header>
+    </div>
+);
 
 export default Home;
 
