0151b509a11bc25f15c66b2eed1fca06d8850055
Austin Wagner <austin.e.wagner@gmail.com>
2018-02-27 18:56:15 -0800

    First commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component }from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <Router>
+        <div>
+          <Nav 
+          
+          />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/about" component={About} />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/Cart" component={Cart} />
+            <Route component={NoMatch} /> */}
+          </Switch>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+    );
+  }
+}
+export default App;
+
---
+++ portfolio/src/components/Nav/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = (props) =>
+  <nav className="navbar navbar-expand-lg navbar-dark navbar-top justify-content-end bg-transparent">
+    <div className="container-fluid">
+      <Link to="/" className="navbar-brand text-danger">
+     {/* <img src="./img//tgb-logo-03.png" alt="The Good Book"/> */}
+        AW
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link text-light" to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-light" to="/products">Portfolio</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-light" to="/cart">Contact</Link>
+          </li>
+        </ul>
+        {/* <span 
+        className="navbar-text">
+          {props.userStatus ? 
+          <div>
+            <img src={props.userStatus.photoURL} className="user-pic-thumb" alt="Current User"/>
+            <button onClick={props.logout} className="btn btn-primary sanch-button">Logout</button></div>
+          :
+          <button onClick={props.login} className="btn btn-primary sanch-button">Login</button>}
+        </span> */}
+      </div>
+    </div>
+  </nav>;
+
+export default Nav;
